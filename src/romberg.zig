89fb421551d6779fb66764a723b840e6b8b98ba9
yctai1994 <yctai.git@gmail.com>
2024-11-18 23:18:32 +0800

    edit: `Romberg.integrate`

--- src/romberg.zig
+++ src/romberg.zig
@@ -1,18 +1,19 @@
 fn Romberg(KMAXX: usize) type {
     if (KMAXX < 2) @compileError("KMAXX should be at least 2.");
-    if (KMAXX & 1 == 1) @compileError("KMAXX should be even.");
 
     const slice_al: comptime_int = @alignOf([]f64);
     const child_al: comptime_int = @alignOf(f64);
     const slice_sz: comptime_int = @sizeOf(usize) * 2;
     const child_sz: comptime_int = @sizeOf(f64);
-    const coeff_sz: comptime_int = (KMAXX + 1) * (KMAXX >> 1) * child_sz + KMAXX * slice_sz;
+    const coeff_sz: comptime_int = ((KMAXX * KMAXX + KMAXX) >> 1) * child_sz + KMAXX * slice_sz;
 
     return struct {
         coeff: [][]f64,
         table: []f64,
 
         const Self = @This();
+
+        const Method = enum { once, iter };
 
         fn init(allocator: mem.Allocator) !*Self {
             const self: *Self = try allocator.create(Self);
@@ -65,19 +66,36 @@ fn Romberg(KMAXX: usize) type {
             allocator.destroy(self);
         }
 
-        fn integrate(self: *const Self, f: *const fn (x: f64) f64, a: f64, b: f64) f64 {
-            for (self.table, 0..) |*p, n| p.* = trapezoidal_sum(f, a, b, (n + 1) << 1);
+        fn integrate(self: *const Self, comptime method: Method, f: *const fn (x: f64) f64, a: f64, b: f64) f64 {
+            switch (method) {
+                .once => {
+                    for (0..KMAXX) |n| self.table[n] = trapezoidal_sum(f, a, b, (n + 1) << 1);
 
-            const offset: f64 = self.table[KMAXX - 1];
-            for (self.table) |*p| p.* -= offset;
+                    const offset: f64 = self.table[KMAXX - 1];
+                    for (0..KMAXX) |n| self.table[n] -= offset;
 
-            for (1..KMAXX) |k| {
-                for (0..KMAXX - k, 1..) |i, ip1| {
-                    self.table[i] += self.coeff[k][i] * (self.table[i] - self.table[ip1]);
-                }
+                    for (1..KMAXX) |k| {
+                        for (0..KMAXX - k, 1..) |i, ip1| {
+                            self.table[i] += self.coeff[k][i] * (self.table[i] - self.table[ip1]);
+                        }
+                    }
+
+                    return offset + self.table[0];
+                },
+                .iter => {
+                    self.table[0] = trapezoidal_sum(f, a, b, (0 + 1) << 1);
+                    for (1..KMAXX) |n| {
+                        self.table[n] = trapezoidal_sum(f, a, b, (n + 1) << 1);
+                        var k: usize = n - 1;
+                        while (true) : (k -= 1) {
+                            self.table[k] += self.coeff[n - k][k] * (self.table[k] - self.table[k + 1]);
+                            if (k == 0) break;
+                        }
+                    }
+
+                    return self.table[0];
+                },
             }
-
-            return offset + self.table[0];
         }
     };
 }
@@ -85,13 +103,11 @@ fn Romberg(KMAXX: usize) type {
 test "Romberg" {
     const page = testing.allocator;
 
-    const self = try Romberg(4).init(page);
+    const self = try Romberg(8).init(page);
     defer self.deinit(page);
 
-    for (self.coeff) |row| debug.print("{any}\n", .{row});
-
-    const result: f64 = self.integrate(func, 0.0, 2.5);
-    debug.print("{d}\n", .{result});
+    debug.print("{d}\n", .{self.integrate(.once, func, 0.0, 2.5)});
+    debug.print("{d}\n", .{self.integrate(.iter, func, 0.0, 2.5)});
 }
 
 fn trapezoidal_sum(f: *const fn (x: f64) f64, a: f64, b: f64, n: usize) f64 {
